4171ac390bb8dcf9219b48060fd9e5b69005d623
justcallmebeef <bethanymitch10@gmail.com>
2021-03-02 14:32:05 -0700

    updated queries and index

--- index.js
+++ index.js
@@ -10,4 +10,25 @@ app.get("/", (req, res, next) => {
     .catch(next);
 });
 
+app.get("/:id", (req, res, next) => {
+  queries
+    .getItemsById(req.params.id)
+    .then((itemById) => res.json(itemById))
+    .catch(next);
+});
+
+app.post("/", (req, res) => {
+  queries.createItem(req.body).then((newItem) => res.json(newItem[0]));
+});
+
+app.delete("/:id", (req, res) => {
+  queries.deleteItem(req.params.id).then(res.sendStatus(204));
+});
+
+app.put("/:id", (req, res) => {
+  queries
+    .updateItem(req.params.id, req.body)
+    .then((updatedItem) => res.json(updatedItem[0]));
+});
+
 app.listen(port);
--- queries.js
+++ queries.js
@@ -4,4 +4,16 @@ module.exports = {
   listAllItems() {
     return database("items");
   },
+  getItemsById(id) {
+    return database("items").where("id", id).first();
+  },
+  createItem(item) {
+    return database("items").insert(item).returning("*");
+  },
+  deleteItem(id) {
+    return database("items").where("id", id).delete();
+  },
+  updateItem(id, item) {
+    return database("items").where("id", id).update(item).returning("*");
+  },
 };
