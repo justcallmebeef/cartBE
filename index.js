bf8802a8a98fceef588c52460497fd2fe64531a2
justcallmebeef <bethanymitch10@gmail.com>
2021-03-02 14:20:07 -0700

    updated get request

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 const express = require("express");
 const app = express();
 const queries = require("./queries");
-// const morgan = require('morgan')
-// const bodyParser  = require('body-parser')
-// const cors = require('cors')
 const port = process.env.PORT || 3002;
 
-// app.use(morgan(process.env.NODE_ENV !== 'production' ? 'dev' : 'combined'))
-// app.use(bodyParser.json())
-// app.use(cors())
-
-app.get("/", (req, res) => {
-  res.send(queries.listAllItems().then((items) => res.send(items)));
+app.get("/", (req, res, next) => {
+  queries
+    .listAllItems()
+    .then((items) => res.send(items))
+    .catch(next);
 });
 
 app.listen(port);
