9c92e631304a05d958c703d869b3cb7737a7bd92
justcallmebeef <bethanymitch10@gmail.com>
2021-03-03 13:39:04 -0700

    installed cors

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const queries = require("./queries");
 const port = process.env.PORT || 3002;
+const cors = require("cors");
+
+app.use(cors());
 
 app.get("/", (req, res, next) => {
   queries
